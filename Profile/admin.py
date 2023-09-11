b2a40e258b6b316c1a50e87cefc8d12c73757791
KollihTheGuruh <mmike8449@gmail.com>
2023-09-11 20:50:09 +0300

    django

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image_url')
+ 
+    
 # Register your models here.
+admin.site.register(models.User, UserAdmin)
--- Profile/views.py
+++ Profile/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import User
+  
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Monday")
+    data = User.objects.all()
+    return render(request, 'index.html', {'data':data})
